921286a4f421d839c7ba2ebe51cf214565174e18
Kenshia <Kenshia@users.noreply.github.com>
2020-10-27 21:14:45 +0700

    Progress (23/10/2020)
    It was previously named something else but i dont think it matters?

---
+++ TheProject/Assets/AmmoCount.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoCount : MonoBehaviour
+{
+    public Image[] ammo;
+    public int ammoRemaining;
+
+    public void LoseAmmo()
+    {
+        if (ammoRemaining == 0) return;
+
+        //Decrease the ammoRemaining
+        ammoRemaining--;
+        //Hide one of the ammo
+        ammo[ammoRemaining].enabled = false;
+
+        //if ammo=0, lose game
+        if (ammoRemaining == 0)
+        {
+            Debug.Log("Lose");
+        }
+    }
+}
---
+++ TheProject/Assets/HitReg.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitReg : MonoBehaviour
+{
+    public AmmoCount ammo;
+    private Crosshair crosshair;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ammo.LoseAmmo();
+            //make some color when pressed or a instantiate
+        }
+    }
+}
