3eeef729ce546f312c621b18a4c2a36335e516f9
javin1102 <61945507+javin1102@users.noreply.github.com>
2020-11-21 16:48:49 +0700

    Revert "Test"
    This reverts commit d9fd0171c5a262d3ed6fcec68924c6769a6fd9f8.

--- TheProject/Assets/Crosshair.cs
+++ TheProject/Assets/Crosshair.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Crosshair : MonoBehaviour
 {
-    public Camera _camera;
     public GameObject crosshair;
     private Vector3 target;
     // Start is called before the first frame update
@@ -16,7 +15,7 @@ public class Crosshair : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        target = _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
         crosshair.transform.position = new Vector2(target.x, target.y);
     }
 }
